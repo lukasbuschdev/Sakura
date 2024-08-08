a56c17c72f88a1ee14661458cd2ed9383cf44027
lukasbuschdev <buschlukas175@gmail.com>
2024-08-08 14:07:10 -0600

    completed our-menu section

---
+++ src/app/main-content/our-menu/meals/meals.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meals',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './meals.component.html',
+  styleUrl: './meals.component.scss'
+})
+
+export class MealsComponent {
+  meals = [
+    {
+      name: "Tonkasu Ramen",
+      img: "../../../../assets/img/r1.png"
+    },
+    {
+      name: "Spicy Miso Ramen",
+      img: "../../../../assets/img/r2.png"
+    },
+    {
+      name: "Shio Ramen",
+      img: "../../../../assets/img/r3.png"
+    }
+  ]
+}
--- src/app/main-content/our-menu/our-menu.component.ts
+++ src/app/main-content/our-menu/our-menu.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MealsComponent } from "./meals/meals.component";
 
 @Component({
   selector: 'app-our-menu',
   standalone: true,
-  imports: [],
+  imports: [MealsComponent],
   templateUrl: './our-menu.component.html',
   styleUrl: './our-menu.component.scss'
 })
+
 export class OurMenuComponent {
 
 }
