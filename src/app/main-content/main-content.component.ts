40c542ca43b863cebcb5f8422789fcd4aff2644d
lukasbuschdev <buschlukas175@gmail.com>
2024-08-08 17:23:44 -0600

    fixed structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from "./landingPage/landingPage.component";
 import { OurMenuComponent } from "./our-menu/our-menu.component";
 import { HowToOrderComponent } from "./how-to-order/how-to-order.component";
-import { FooterComponent } from "../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-main-content',
@@ -10,8 +9,7 @@ import { FooterComponent } from "../shared/components/footer/footer.component";
   imports: [
     LandingPageComponent,
     HowToOrderComponent,
-    OurMenuComponent,
-    FooterComponent
+    OurMenuComponent
 ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
